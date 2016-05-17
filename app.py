f337df5fc92c285b8d44910b5735dbb57a0a8deb
serenadipity <serenagchan@gmail.com>
2016-05-16 22:57:06 -0400

    stuff

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.secret_key = "Password"
+    app.run(host='0.0.0.0', port=8000)
