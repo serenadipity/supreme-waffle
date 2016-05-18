515def73f108884d99ae9870c78c94458c383527
serenadipity <serenagchan@gmail.com>
2016-05-18 12:21:10 -0400

    stuff

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.secret_key = "Password"
