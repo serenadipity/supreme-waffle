37a5c1cd13e0a4b9bc51305c4288d1dffe3d7ee3
alicexue <alicex123@gmail.com>
2016-05-24 23:36:33 -0400

    remove test cases

--- datas.py
+++ datas.py
@@ -285,9 +285,7 @@ def get_gamescores_by_school(school):
         total_score += game[0]
     return [num_games, total_score]
 
-create_event("Stuyvesant High School", 20, "Bronx Science", 15, "02/15/2016", "4:00pm", 22745, "", "345 Chambers Street")
-
-create_event("Brooklyn Tech", 35, "Stuyvesant High School", 20, "04/25/2016", "5:30pm", 36375, "", "345 Chambers Street")
-
-print get_gamescores_by_school("Stuyvesant High School")
+#create_event("Stuyvesant High School", 20, "Bronx Science", 15, "02/15/2016", "4:00pm", 22745, "", "345 Chambers Street")
+#create_event("Brooklyn Tech", 35, "Stuyvesant High School", 20, "04/25/2016", "5:30pm", 36375, "", "345 Chambers Street")
+#print get_gamescores_by_school("Stuyvesant High School")
 
