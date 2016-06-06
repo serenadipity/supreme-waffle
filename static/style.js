43160b4e0d857efeeed913c87b7c9c9bf17aa172
serenadipity <serenagchan@gmail.com>
2016-06-06 12:11:04 -0400

    idk

--- static/style.js
+++ static/style.js
@@ -3,8 +3,10 @@ $('#myTabs a').click(function (e) {
   $(this).tab('show')
 })
 
+$('#myTabs a[href="#upcoming"]').tab('show') // Select tab by name
 /*
 $('#myTabs a[href="#foil"]').tab('show') // Select tab by name
 $('#myTabs a:first').tab('show') // Select first tab
 $('#myTabs a:last').tab('show') // Select last tab
 $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+*/
