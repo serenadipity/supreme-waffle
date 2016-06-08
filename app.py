5825242f7cb704556bd71e28c7c2c3464915144b
Sammi <sammiwuleung@gmail.com>
2016-06-08 12:25:51 -0400

    better times

--- app.py
+++ app.py
@@ -166,8 +166,10 @@ def register_player():
 def show_school_profile(school_name):
     if 'user' not in session:
         session['user'] = 0
-    else:
+    if session['user'] != 0:
         user_school = get_user_school(session['user'])
+    else:
+        user_school = ""
     user = session['user']
 
     result = get_school(school_name)
@@ -250,8 +252,10 @@ def show_player_profile(year, id):
     #look up player
     if 'user' not in session:
         session['user'] = 0
-    else:
+    if session['user'] != 0:
         user_school = get_user_school(session['user'])
+    else:
+        user_school = ""
     user = session['user']
     player = get_player(year,id)
     image = get_player_image(id, year)
