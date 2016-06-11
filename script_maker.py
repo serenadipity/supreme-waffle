4e326366ba3ed068a092b947951ab474a160675a
Sammi <sammiwuleung@gmail.com>
2016-06-11 14:50:02 -0400

    prevent future events from adding data in script

--- script_maker.py
+++ script_maker.py
@@ -1,7 +1,7 @@
 
 from datas import *
 from random import randint
-
+import time
 create_all_tables()
 
 ######### MAKE SCHOOLS #########
@@ -77,12 +77,13 @@ for i in range(30):
             gender = "Boys"
         away_score = 0
         home_score = 0
-        for i in range(1,10):
-            p1 = playersH[randint(1,len(playersH)-1)][1]
-            p2 = playersA[randint(1,len(playersA)-1)][1]
-            home_touches = randint(0, ((9 * i) - home_score))
-            home_score += home_touches
-            away_touches = randint(0, ((9 * i) - away_score))
-            away_score += away_touches
-            create_ind(schoolH, p1, home_touches, home_score, schoolA, p2, away_touches, away_score, date, gametype, game, i, address, 2016, gender)
+        if date < time.strftime("%m/%d/%Y"):
+            for i in range(1,10):
+                p1 = playersH[randint(1,len(playersH)-1)][1]
+                p2 = playersA[randint(1,len(playersA)-1)][1]
+                home_touches = randint(0, ((9 * i) - home_score))
+                home_score += home_touches
+                away_touches = randint(0, ((9 * i) - away_score))
+                away_score += away_touches
+                create_ind(schoolH, p1, home_touches, home_score, schoolA, p2, away_touches, away_score, date, gametype, game, i, address, 2016, gender)
     
